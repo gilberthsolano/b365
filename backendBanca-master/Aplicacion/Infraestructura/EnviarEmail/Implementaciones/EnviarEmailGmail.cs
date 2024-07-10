5afecb78823c8a97d5f8a1eda57b5c7c62254525
michael-jmz <www.michaelj@hotmail.es>
2024-07-09 21:44:16 -0500

    estructura del proyecto

---
+++ backendBanca-master/Aplicacion/Infraestructura/EnviarEmail/Implementaciones/EnviarEmailGmail.cs
@@ -0,0 +1,13 @@
+﻿using Aplicacion.Infraestructura.EnviarEmail.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Infraestructura.EnviarEmail.Implementaciones
+{
+    public class EnviarEmailGmail:IEnviarEmail
+    {
+    }
+}
