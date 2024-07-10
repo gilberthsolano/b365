89a907d48f8da10abf5a4bd6bddea66d05738fd8
michael-jmz <www.michaelj@hotmail.es>
2024-07-09 22:46:35 -0500

    actualizacion de controladores

--- backendBanca-master/ApiBanca/Controllers/RegistroCivilController.cs
+++ backendBanca-master/ApiBanca/Controllers/RegistroCivilController.cs
@@ -20,28 +20,9 @@ namespace ApiBanca.Controllers
             {
                 return BadRequest(new { Mensaje = error.Message, error.Errors });
             }
-            catch (Exception error)
-            {
-                return BadRequest(new { Mensaje = error.Message });
-            }
+            catch (Exception error){
+                return BadRequest(new { Mensaje = error.Message });}
         }
-
-        [HttpPost("validar-rostro")]
-        public async Task<ActionResult> ValidarRostro(ValidarRostro.DatosValidarRostro request)
-        {
-            try
-            {
-                var resultado = await Mediator.Send(new ValidarRostro.Comando(request));
-                return Ok(resultado);
-            }
-            catch (ExcepcionValidacion error)
-            {
-                return BadRequest(new { Mensaje = error.Message, error.Errors });
-            }
-            catch (Exception error)
-            {
-                return BadRequest(new { Mensaje = error.Message });
-            }
-        }
+        // AQUI VA LA VALIDACION DE ROSTRO
     }
 }
